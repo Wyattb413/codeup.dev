c170b596bc88b19c2be190374897eda4e8ef7fb8
Wyatt Barnes <wyattb413@gmail.com>
2016-08-01 11:15:21 -0500

    added some ;

--- public/js/functions.js
+++ public/js/functions.js
@@ -5,11 +5,11 @@ var myNameIs = 'Wyatt'; // TODO: Fill in your name here.
 // TODO:
 // Create a function called 'sayHello' that takes a parameter 'name'.
 // When called, the function should log a message that says hello from the passed in name.
-function sayHello(name) {
-	console.log("Hello " + name)
+function sayHello (name) {
+	console.log("Hello " + name);
 }
 // TODO: Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
-sayHello(myNameIs)
+sayHello(myNameIs);
 // Don't modify the following line
 // It generates a random number between 1 and 100 and stores it in random
 var random = Math.floor((Math.random()*100)+1);
