63568d3ea507c28eeb6e913b1005622a308d3091
Wyatt Barnes <wyattb413@gmail.com>
2016-08-02 08:56:09 -0500

    Added Functions

--- public/js/if-else.js
+++ public/js/if-else.js
@@ -2,52 +2,61 @@
 
 //Grade Problem
 
-var gradeAvg=((70 + 80 + 95) / 3)
+    function gradeAvg (grade1, grade2, grade3) {
+        var gradeAvg = ((grade1 + grade2 + grade3) / 3);
+        if (gradeAvg >= 80) {
+            return "You're Awesome!";
+        } else {
+            return "You Need to Practice More!";
+        }
+    }
+    console.log(gradeAvg(75, 90, 80));
+    console.log(gradeAvg(25, 50, 60));
+    console.log(gradeAvg(80, 15, 78));
 
-if (gradeAvg >= 80) {
-	console.log("You're Awesome!")
-} else {
-	console.log("You Need to Practice More!")
-}
 
 //Heb Problem
 
-var amountRyanPaid=250
-var amountCameronPaid=180
-var amountGeorgePaid=320
-var discount= .35
-var total;
+    var total;
 
-if (amountRyanPaid > 200) {
-	total=(amountRyanPaid - (amountRyanPaid * discount)) 
-	console.log("Amount Ryan Owes" + " $" + amountRyanPaid + ", Discount was Applied" + ", Final Payment: " + "$" + parseFloat(total).toFixed(2)) 
-} else {
-	total=(amountRyanPaid)
-	console.log("Amount Ryan Owes" + " $" + amountRyanPaid + ", Discount was Not Applied" + ", Final Payment: " + "$" + total) 
-}
-
-if (amountCameronPaid > 200) {
-	total=(amountCameronPaid - (amountCameronPaid * discount)) 
-	console.log("Amount Cameron Owes" + " $" + amountCameronPaid + ", Discount was Applied" + ", Final Payment: " + "$" + total) 
-} else {
-	total=(amountCameronPaid)
-	console.log("Amount Cameron Owes" + " $" + amountCameronPaid + ", Discount was Not Applied" + ", Final Payment: " + "$" + total) 
-}
-
-if (amountGeorgePaid > 200) {
-	total=(amountGeorgePaid - (amountGeorgePaid * discount)) 
-	console.log("Amount George Owes" + " $" + amountGeorgePaid + ", Discount was Applied" + ", Final Payment: " + "$" + total) 
-} else {
-	total=(amountGeorgePaid - (amountGeorgePaid * discount)) 
-	console.log("Amount Ryan Owes" + " $" + amountRyanPaid + ", Discount was Not Applied" + ", Final Payment: " + "$" + total) 
-}
+    //Function for deciding whether or not discount will be applied
+    function wasDicountApplied (amountPaid, nameOfCustomer, discount, discountBreakPoint) {
+        if (amountPaid > discountBreakPoint) {
+            total = (amountPaid - (amountPaid * discount)) 
+            // console.log("Amount " + nameOfCustomer + " Owes" + " $" + amountPaid + ", Discount was Applied" + ", Final Payment: " + "$" + parseFloat(total).toFixed(2)) 
+            return "Amount " + nameOfCustomer + " Owes" + " $" + amountPaid + ", Discount was Applied" + ", Final Payment: " + "$" + parseFloat(total).toFixed(2)
+        } else {
+            total = (amountPaid)
+            // console.log("Amount " + nameOfCustomer + " Owes" + " $" + amountPaid + ", Discount was Not Applied" + ", Final Payment: " + "$" + total)
+            return "Amount " + nameOfCustomer + " Owes" + " $" + amountPaid + ", Discount was Not Applied" + ", Final Payment: " + "$" + total
+        }
+    }
+    //Displaying indivisual customer's total
+    var ryan = wasDicountApplied (250, "Ryan", .35, 200);
+    console.log(ryan)
+    var cameron = wasDicountApplied (180, "Cameron", .35, 200);
+    console.log(cameron)
+    var george = wasDicountApplied (320, "George", .35, 200);
+    console.log(george)
 
 //Friend Problem
+    function shouldBuyHouseOrCar() {
+        if (flipACoin() == 1) {
+            return("Buy a House")
+        } else {
+            return("Buy a Car")
+        }
+    }
+    function flipACoin() {
+        var flipACoin = Math.floor(Math.random()* 2);
+        return flipACoin;
+    }
+    console.log(shouldBuyHouseOrCar());
 
-var flipACoin = Math.floor(Math.random()* 2)
 
-if (flipACoin == 1) {
-	console.log("Buy a House")
-} else {
-	console.log("Buy a Car")
-}
+
+
+
+
+
+
